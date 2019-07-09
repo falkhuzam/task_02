96c17e7491547721d513059039e4bd7c0e8d3e24
Fahad AlKhuzam <falkhuzam@Fahads-MacBook-Air.local>
2019-07-09 17:46:20 +0300

    task_02

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def favourite_restaurants(request):
+    return render(request, "pizza.html")
--- task_02/urls.py
+++ task_02/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from restaurants.views import favourite_restaurants
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',,name='hello-world'),
+    path('restaurants/',favourite_restaurants,name='hello-world'),
 ]
